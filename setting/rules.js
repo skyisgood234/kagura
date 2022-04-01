c6514eb031cf58ddde76ce7d487c76a30c53d819
skyisgood234 <skyisgood321@gmail.com>
2022-04-01 09:54:39 +0700

    kagura

---
+++ setting/rules.js
@@ -0,0 +1,23 @@
+/*Kalian bisa ganti ini rulesnya
+ingat !! yang ${botname} jangan di ganti
+biarin aja sans ntar bakalan keluar nama bot luh kok
+*/
+const rules = (prefix, botname) => { 
+return `*_RULES BOT_*
+
+1. Jangan spam bot. 
+Sanksi: *GUA BLOK AJG*
+
+2. Jangan telepon bot.
+Sanksi: *GUA BLOK TAPI KLO CEWE GPP*
+
+3. Jangan memnghina/menghujat bot.
+Sanksi: *GUA BLOK NOMER LU + WAR SANTED*
+
+_BOT NYOLONG SC ORG/AUTO RES/WIBU AE BANGGA JANGAN SOK PRO DEK !_
+
+Jika sudah dipahami rules-nya, silakan ketik *${prefix}menu* untuk memulai!
+
+──「 *${botname}* 」──`
+}
+exports.rules = rules
